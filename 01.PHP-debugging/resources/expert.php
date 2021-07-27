a08fc492726d42f511a3cbe566cf4112f36c4efc
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-07-27 11:00:52 +0200

    adjusted exercise 1 and added exercise 2

--- 01.PHP-debugging/resources/expert.php
+++ 01.PHP-debugging/resources/expert.php
@@ -1,3 +1,32 @@
 <?php
 
 declare(strict_types=1);
+
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+//Exercise 1
+
+function new_exercise($x) {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+}
+
+new_exercise(1);
+
+
+//Exercise 2
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
+//Exercise 3
+
+$str = “Debugged ! Also very fun”;
+echo substr($str, 0, 10);
